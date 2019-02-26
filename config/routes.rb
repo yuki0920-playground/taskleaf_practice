719f8168e51a9993385d345bcd2f72e82827d656
Yuki <ywsep20@yahoo.co.jp>
2019-02-26 13:34:26 +0000

    database&index,new,create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :tasks
+  root to: 'tasks#index'
+  # get 'index/show'
+  # get 'index/new'
+  # get 'index/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
