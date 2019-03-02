0c6b2e13f6b97398de9a3e08aa69c6c7c54867e8
Yuki <ywsep20@gmail.com>
2019-03-02 09:40:21 +0000

    新規作成機能テスト追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,5 @@
 class Task < ApplicationRecord
-  validates :name, presence: true
-  validates :name, length: { maximum: 30}
+  validates :name, presence: true, length: { maximum: 30 }
   validate :validate_name_not_including_comma
   
   belongs_to :user
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -12,13 +12,15 @@ describe 'タスク管理機能', type: :system do
     click_button 'ログインする'
   end
   
+  shared_examples_for 'ユーザーAが作成したタスクが表示される' do
+    it { expect(page).to have_content '最初のタスク'}
+  end
+  
   describe '一覧表示機能' do
     context 'ユーザーAがログインしているとき' do
       let(:login_user) { user_a }
       
-      it 'ユーザーAが作成したタスクが表示される' do
-        expect(page).to have_content '最初のタスク'
-      end
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
     end
       
     context 'ユーザーBがログインしているとき' do
@@ -38,9 +40,34 @@ describe 'タスク管理機能', type: :system do
         visit task_path(task_a)
       end
       
-      it 'ユーザーAが作成したタスクが表示される' do
-        expect(page).to have_content '最初のタスク'
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
+    end
+  end
+  
+  describe '新規作成機能' do
+    let(:login_user) { user_a }
+    
+    before do
+      visit new_task_path
+      fill_in '名称', with: task_name
+      click_button '登録する'
+    end
+    
+    context '新規作成画面で名称を入力したとき' do
+      let(:task_name) { '新規作成のテストを書く' }
+      
+      it '正常に登録される' do
+        expect(page).to have_selector '.alert-success', text: '新規作成のテストを書く'
       end
     end
+    # context '新規作成画面で名称を入力しなかったとき' do
+    #   let(:task_name) { '' }
+
+    #   it 'エラーとなる' do
+    #     within '#error_explanation' do
+    #       expect(page).to have_content "Name can't be blank"
+    #     end
+    #   end
+    # end
   end
 end
